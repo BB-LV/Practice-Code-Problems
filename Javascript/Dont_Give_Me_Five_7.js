df716b72b7c85a8ef72a45c6db0db30fe1b9ac8f
brian.beardsley.89123@gmail.com <94084517+Bevelled@users.noreply.github.com>
2022-05-18 02:46:22 -0700

    Added Dont_Give_Me_5_7

---
+++ Javascript/Dont_Give_Me_Five_7.js
@@ -0,0 +1,39 @@
+/*
+Dont Give me Five! 7kyu
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+
+I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+*/
+
+/* Strategy:
+    The input is two numbers and the answer requests us to give us the amount of numbers
+        between these two inputs, that dont have a 5 in them. 
+    An easy way to achieve this is to use the .includes method on each number, the number
+    would have to be converted to a string first.
+    I will use a for loop to loop through numbers and add those numbers to an array. I will 
+    only add those numbers if the (does not)!.includes('5') method returns positive for the number.
+    Finally I'll return the length of the created array.
+*/
+
+function dontGiveMeFive(start, end){
+    let numSplit = []
+    for(let i = start; i <= end; i++){
+      let e = String(i).split('');
+      if(!e.includes('5')){
+        numSplit.push(i)
+      }
+    }
+    return numSplit.length;
+  }
