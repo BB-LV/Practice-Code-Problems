57cc185a382d07b74b61ca9c69501d4b85fa5e3f
brian.beardsley.89123@gmail.com <94084517+Bevelled@users.noreply.github.com>
2022-05-19 04:04:33 -0700

    Added Drying_Potatoes_7

---
+++ Javascript/Drying_Potatoes_7.js
@@ -0,0 +1,48 @@
+/*
+Drying Potatoes 7kyu
+
+John bought potatoes: their weight is 100 kilograms. Potatoes contain water and dry matter. The water content is 99 percent of the total weight. He thinks they are too wet and puts them in an oven - at low temperature - for them to lose some water.
+
+At the output the water content is only 98%.
+
+What is the total weight in kilograms (water content plus dry matter) coming out of the oven?
+
+He finds 50 kilograms and he thinks he made a mistake: "So much weight lost for such a small change in water content!"
+
+Can you help him?
+Task
+
+Write function potatoes with
+
+    int parameter p0 - initial percent of water-
+    int parameter w0 - initial weight -
+    int parameter p1 - final percent of water -
+
+potatoesshould return the final weight coming out of the oven w1 truncated as an int.
+*/
+
+/* (P.R.E.P(Parameters, Returns, Examples, Pseudo-Code))
+    Parameters = 3 integers. p0 & p1 will be percentages, w0 will be positive whole or float number. 
+    int parameter p0 - initial percent of water-
+    int parameter w0 - initial weight -
+    int parameter p1 - final percent of water -
+
+
+    Returns = The remainder of the 
+
+    Examples =      potatoes(82, 127, 80) // 114);
+                    potatoes(93, 129, 91) // 100);
+                    potatoes(99, 100, 98) // 50;
+
+    Pseudo-Code = Description
+*/
+
+/* First Attempt
+
+attempt
+
+*/
+
+// How to Clean Up
+
+// Final Answer
