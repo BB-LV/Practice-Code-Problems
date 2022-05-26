9d94db4c605adff48834f0625f6f6bbcc8397b7d
brian.beardsley.89123@gmail.com <94084517+Bevelled@users.noreply.github.com>
2022-05-25 17:48:09 -0700

    Added a test file

---
+++ Javascript/template.js
@@ -0,0 +1,29 @@
+/*
+Name #kyu
+
+Description
+*/
+
+/* (P.R.E.P(Parameters, Returns, Examples, Pseudo-Code))
+    Parameters = (number of inputs, data type of inputs[Number: Whole Number / Floated, Positive / Negative])
+
+    Returns = (where to return the answer to, data type/s of answer, how many answers to return)
+
+    Examples = functionName(input){
+
+                    }
+                    functionName(input1) // answer1
+                    functionName(input2) // answer2
+                    functionName(input3) // answer3
+    Pseudo-Code = Description
+*/
+
+/* Initial Attempt
+
+attempt
+
+*/
+
+// How to Clean Up
+
+// Final Answer
